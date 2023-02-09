5e6f8f39260cc5000f9881259613dd42d5f1fb9a
wtc <wtc@123456.com>
2023-02-09 15:32:59 +0800

    dev

--- cpp11/type_traits/tt.cpp
+++ cpp11/type_traits/tt.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <type_traits>
 
+struct AAA 
+{
+};
+
+struct BBB
+{
+    BBB () = delete;
+};
+
 int
 main ()
 {
@@ -8,4 +17,7 @@ main ()
     std::cout << std::is_scalar_v<int *> << std::endl;
     std::cout << std::is_scalar_v<const int *> << std::endl;
     std::cout << std::is_scalar_v<std::byte> << std::endl;
+
+    std::cout << std::is_constructible_v<AAA> << std::endl;
+    std::cout << std::is_constructible_v<BBB> << std::endl;
 }
