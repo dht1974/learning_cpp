4109cf621e2367ba0a4cbdf5d7caa74150e8e8f9
wtc <wtc@123456.com>
2023-02-09 09:56:27 +0800

    add thread_local

---
+++ thread/thread_local.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+ 
+/// https://en.cppreference.com/w/cpp/language/storage_duration
+
+/// thread_local 就是把 gcc __thread 进行了标准化，thread_local的变量只在本线程中
+/// 可见，正常情况下，线程是共享内存空间的
+
+/// 下面两句效果相同
+/// __thread unsigned int rage = 1; 
+thread_local unsigned int rage = 1; 
+std::mutex cout_mutex;
+ 
+void increase_rage(const std::string& thread_name)
+{
+    for (int i = 0; i < 100000000; ++i) {
+        ++rage; // modifying outside a lock is okay; this is a thread-local variable
+    }
+    std::lock_guard<std::mutex> lock(cout_mutex);
+    std::cout << "Rage counter for " << thread_name << ": " << rage << '\n';
+}
+ 
+int main()
+{
+    std::thread a(increase_rage, "a"), b(increase_rage, "b");
+ 
+    {
+        std::lock_guard<std::mutex> lock(cout_mutex);
+        std::cout << "Rage counter for main: " << rage << '\n';
+    }
+ 
+    a.join();
+    b.join();
+}
